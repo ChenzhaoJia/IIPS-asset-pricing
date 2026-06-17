74c169997d33147cd20b6f7b2ce88c099fde887c
ChenzhaoJia <jiachenzhao4@gmail.com>
2026-06-17 23:03:08 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="esi_asset_pricing",
+    version="0.1.0",
+    description="Empirical Asset Pricing with Effective Soothing Index (ESI)",
+    author="Your Name",
+    packages=find_packages(),
+)
